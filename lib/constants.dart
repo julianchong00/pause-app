d8496f34f09844ffecc895e36d784a5eb5f31c26
Julian Chong <26028372+julianchong00@users.noreply.github.com>
2026-04-18 10:01:52 +1000

    feat: add curated supported currencies list

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+const kDefaultCurrency = 'USD';
+
+const kSupportedCurrencies = <String>[
+  'USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'NZD', 'SGD',
+  'INR', 'HKD', 'KRW', 'SEK', 'NOK', 'DKK', 'MXN', 'BRL', 'ZAR', 'THB',
+  'MYR', 'IDR', 'PHP', 'VND', 'AED', 'SAR', 'TRY', 'PLN', 'CZK', 'ILS',
+];
